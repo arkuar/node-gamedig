9134f429df6b3d25b26c72d0691ad6d79ec273eb
arkuar <arttukuronen@gmail.com>
2020-10-25 17:35:49 +0200

    Count Epic players for RS2

--- protocols/rs2.js
+++ protocols/rs2.js
@@ -6,7 +6,13 @@ class Rs2 extends Valve {
     }
 
     async cleanup(state) {
-        state.players = state.raw.players;
+        // Epic players are not included in the players array but can be found in rules.
+        const epicPlayers = Object.keys(state.raw.rules)
+            .filter((key) => state.raw.rules[key] === "EOS")
+            .map((platformKey) => state.raw.rules[platformKey.replace("PI_P", "PI_N")])
+            .map(player => ({ name: player, score: 0, time: 0 })); // No way to get score or time for epic players?
+
+        state.players = state.raw.players.concat(epicPlayers);
         delete state.raw.players;
     }
 }
